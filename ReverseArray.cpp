b4163e78131d8f162844c5354a61365723d633d3
ccampbell97 <47377048+ccampbell97@users.noreply.github.com>
2019-02-07 19:50:39 -0800

    Add files via upload

---
+++ ReverseArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void reverseArr(int *arr1, int end)
+{
+	int start = 0;
+	while (start < end)
+	{
+		int temp = arr1[start];
+		arr1[start] = arr1[end];
+		arr1[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+void printArr(int *arr1, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr1[i] << endl;
+}
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5 };
+	int i;
+	printArr(arr1, 5);
+	reverseArr(arr1, 4);
+	printArr(arr1, 5);
+}
